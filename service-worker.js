e4264d78c592ba83bf2ece3faa422bf7081683eb
Ayesh Alshukri <aalshukri@googlemail.com>
2023-07-14 14:28:17 +0100

    bugfix for style name in asset/url list.

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@ var VERSION = 'version_001';
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
-  `${GHPATH}/css/styles.css`,
+  `${GHPATH}/css/style.css`,
   `${GHPATH}/js/app.js`,
   `${GHPATH}/img/icon.png`,
 ] 
